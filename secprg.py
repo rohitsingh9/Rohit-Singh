47920c70146e1cdb30d4f830e6b0ee5bf7931bba
rohitsingh9 <36465429+rohitsingh9@users.noreply.github.com>
2018-02-19 09:41:48 +0530

    second prg

---
+++ secprg.py
@@ -0,0 +1,98 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+0
+1
+2
+3
+4
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+0
+1
+2
+3
+4
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+0
+1
+2
+3
+4
+1
+3
+5
+7
+9
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+0
+1
+2
+3
+4
+c value end
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+0
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+0
+1
+2
+3
+4
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+0
+1
+2
+3
+4
+value ended
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+2
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+2
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+Traceback (most recent call last):
+  File "/home/cs2017a210/rohitsingh/secprg.py", line 4, in <module>
+    print(addition(a,b),a+b)
+NameError: name 'a' is not defined
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+5 5
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+5
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+5
+0
+0
+1
+2
+3
+4
+>>> 
+=============== RESTART: /home/cs2017a210/rohitsingh/secprg.py ===============
+5
+0
+0
+1
+2
+3
+4
+0
+1
+2
+1
+1
+2
+1
+end
+>>> 
