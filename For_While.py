e0e047292273c62f99442bf3690202049b0b4cb6
rohitsingh9 <36465429+rohitsingh9@users.noreply.github.com>
2018-03-23 19:48:25 +0530

    Add files via upload

---
+++ For_While.py
@@ -0,0 +1,33 @@
+x = [25,23,32,12,45]
+x.sort()
+print (x)
+
+for i in x:
+    print(i)
+else:
+    print("No items left.")
+    
+ 
+n=10
+sum=0
+i=1
+while i  <= 10:
+    sum = sum + i
+    i = i + 1
+    print ("The sum is",sum)
+
+
+s = "Noobs\n"
+for i in s :
+    print(i)
+
+for val in "\nstring":
+    if val == 'i':
+        break
+    print (val)
+
+
+
+
+
+
